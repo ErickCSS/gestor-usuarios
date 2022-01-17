49e9bf56d08a3b05e986770b35e91971714afb65
X3n0nm4st3r0403 <erickj.cruzs@gmail.com>
2022-01-16 23:57:31 -0400

    [ADD] App gestor-usuarios

---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+
+
+const Input = ({label, ...props}) => {
+   // Para llamar las propiedas a un función de componentes, siempre debo utilizar un objeto literal ( -- {props} --), osea llamar las llaves
+   return(
+      <div>
+         <label>{label}</label>
+         <input {...props} />
+         {/* Para poder pasar las propiedades de un componente principal en la page App.Js a su etiqueta, lo que debemos hacer es llamar en fomar de spread operator las propiedades, haciendo una copia de ellas "{...props}" */}
+         {/* Estas yo primero tengo que llamarlas y setearlas como parametros de mi función que me crea el componente y luego pasarsela a mi etiqueta que lo lleva. */}
+      </div>
+   )
+}
+
+export default Input
---
+++ src/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const useForm = (inicial) => {
+   const [form, setForm] = useState(inicial)
+   // para que hacer dinamico mi useState, simple a mi función le paso un parametro, y luego ese parametro se lo coloco a useState, así cuando use mi custom hooks en App.js, que le pase los parametros iniciales, directamente llegarán a useState, en este archivo
+   const handleChange = (x) => {
+      setForm({
+         ...form,
+         // Esto es una copia de mi variable de formulario, para hacer que no se me desaparezcan los otros valores iniciales de useState cuando comience a capturar los value
+         [x.target.name]: x.target.value
+      })
+   }
+   console.log(form)
+
+   return [form, handleChange]
+   // Los valores que siempre tengo que exportar cuando hago un custom hooks, siempre será como primer valor, la variable que asignamos en useState y la función que me captura el value de mis inputs, que dentro lleva la función que me actualiza mi variable de useState, que en este caso es "form"
+}
+
+export default useForm
