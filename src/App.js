f2d2d1338c4704ea6afe3206efab372e724f77e6
X3n0nm4st3r0403 <erickj.cruzs@gmail.com>
2022-01-17 00:33:56 -0400

    [ADD] Styles and Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import useForm from './hooks/useForm'
 import Input from  './components/Input'
+import Card from './components/Card'
+import Container from './components/Container'
 
 function App() {
-  const [form, handleChange] = useForm({name: ''})
+  const [form, handleChange] = useForm({name: '', lastname: ''})
   return (
-    <form>
-      <Input value={form.name} onChange={handleChange}></Input>
-    </form>
+    <Container>
+      <Card>
+        <form>
+          <Input label='Nombre' name="name" value={form.name} onChange={handleChange}></Input>
+          <Input label='Apellido' name="lastname" value={form.lastname} onChange={handleChange}></Input>
+          {/* Para que mi componente funcione correctamente siempre debo pasarle la propiedad de " name='' ", y asignarle su valor, ya que es a este a quien le indexaremos el contenido de "value" */}
+        </form>
+      </Card>
+    </Container>
   )
 }
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,9 @@
-
+import './input.css'
 
 const Input = ({label, ...props}) => {
    // Para llamar las propiedas a un función de componentes, siempre debo utilizar un objeto literal ( -- {props} --), osea llamar las llaves
    return(
-      <div>
+      <div className='field'>
          <label>{label}</label>
          <input {...props} />
          {/* Para poder pasar las propiedades de un componente principal en la page App.Js a su etiqueta, lo que debemos hacer es llamar en fomar de spread operator las propiedades, haciendo una copia de ellas "{...props}" */}
